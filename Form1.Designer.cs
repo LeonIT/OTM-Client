d95554509371f8e9acf8ed280aed84c636664f7c
Jarrin van der Velden <j@rrin.nl>
2013-12-16 23:13:41 +0100

    Eerste opzetje

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OTM_Client
 {
-    partial class Form1
+    partial class frm_main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,34 +28,265 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.brn_settings = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnl_dialer = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.pnl_dialer.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // brn_settings
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(169, 60);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.brn_settings.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.brn_settings.FlatAppearance.BorderSize = 2;
+            this.brn_settings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.brn_settings.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brn_settings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.brn_settings.Image = global::OTM_Client.Properties.Resources.framework_settings;
+            this.brn_settings.Location = new System.Drawing.Point(161, 155);
+            this.brn_settings.Name = "brn_settings";
+            this.brn_settings.Size = new System.Drawing.Size(84, 53);
+            this.brn_settings.TabIndex = 24;
+            this.brn_settings.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // btn_0
+            // 
+            this.btn_0.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.btn_0.FlatAppearance.BorderSize = 2;
+            this.btn_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_0.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.btn_0.Location = new System.Drawing.Point(78, 155);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(87, 53);
+            this.btn_0.TabIndex = 23;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.button11.FlatAppearance.BorderSize = 2;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.button11.Image = global::OTM_Client.Properties.Resources.phone_logo_circle_green_kl1;
+            this.button11.Location = new System.Drawing.Point(-3, 155);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(83, 53);
+            this.button11.TabIndex = 22;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // btn_9
+            // 
+            this.btn_9.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.btn_9.FlatAppearance.BorderSize = 2;
+            this.btn_9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.btn_9.Location = new System.Drawing.Point(161, 104);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(84, 53);
+            this.btn_9.TabIndex = 21;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            // 
+            // btn_8
+            // 
+            this.btn_8.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.btn_8.FlatAppearance.BorderSize = 2;
+            this.btn_8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.btn_8.Location = new System.Drawing.Point(78, 104);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(85, 53);
+            this.btn_8.TabIndex = 20;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            // 
+            // btn_7
+            // 
+            this.btn_7.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.btn_7.FlatAppearance.BorderSize = 2;
+            this.btn_7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.btn_7.Location = new System.Drawing.Point(-3, 104);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(83, 53);
+            this.btn_7.TabIndex = 19;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            // 
+            // btn_6
+            // 
+            this.btn_6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.btn_6.FlatAppearance.BorderSize = 2;
+            this.btn_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.btn_6.Location = new System.Drawing.Point(161, 53);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(84, 53);
+            this.btn_6.TabIndex = 18;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.btn_5.FlatAppearance.BorderSize = 2;
+            this.btn_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.btn_5.Location = new System.Drawing.Point(78, 53);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(85, 53);
+            this.btn_5.TabIndex = 17;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            // 
+            // btn_4
+            // 
+            this.btn_4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.btn_4.FlatAppearance.BorderSize = 2;
+            this.btn_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.btn_4.Location = new System.Drawing.Point(-3, 53);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(83, 53);
+            this.btn_4.TabIndex = 16;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            // 
+            // btn_3
+            // 
+            this.btn_3.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btn_3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.btn_3.FlatAppearance.BorderSize = 2;
+            this.btn_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.btn_3.Location = new System.Drawing.Point(161, 2);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(84, 53);
+            this.btn_3.TabIndex = 15;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            // 
+            // btn_2
+            // 
+            this.btn_2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.btn_2.FlatAppearance.BorderSize = 2;
+            this.btn_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.btn_2.Location = new System.Drawing.Point(78, 2);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(85, 53);
+            this.btn_2.TabIndex = 14;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
+            this.btn_1.FlatAppearance.BorderSize = 2;
+            this.btn_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(167)))), ((int)(((byte)(178)))));
+            this.btn_1.Location = new System.Drawing.Point(-3, 2);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(83, 53);
+            this.btn_1.TabIndex = 13;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pnl_dialer);
+            this.panel1.Location = new System.Drawing.Point(3, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 266);
+            this.panel1.TabIndex = 25;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pnl_dialer
+            // 
+            this.pnl_dialer.Controls.Add(this.btn_1);
+            this.pnl_dialer.Controls.Add(this.brn_settings);
+            this.pnl_dialer.Controls.Add(this.btn_2);
+            this.pnl_dialer.Controls.Add(this.btn_0);
+            this.pnl_dialer.Controls.Add(this.btn_3);
+            this.pnl_dialer.Controls.Add(this.button11);
+            this.pnl_dialer.Controls.Add(this.btn_4);
+            this.pnl_dialer.Controls.Add(this.btn_9);
+            this.pnl_dialer.Controls.Add(this.btn_5);
+            this.pnl_dialer.Controls.Add(this.btn_8);
+            this.pnl_dialer.Controls.Add(this.btn_6);
+            this.pnl_dialer.Controls.Add(this.btn_7);
+            this.pnl_dialer.Location = new System.Drawing.Point(3, 58);
+            this.pnl_dialer.Name = "pnl_dialer";
+            this.pnl_dialer.Size = new System.Drawing.Size(241, 213);
+            this.pnl_dialer.TabIndex = 27;
+            this.pnl_dialer.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // frm_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 418);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(58)))), ((int)(((byte)(69)))));
+            this.ClientSize = new System.Drawing.Size(250, 299);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_main";
             this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frm_main_Load);
+            this.panel1.ResumeLayout(false);
+            this.pnl_dialer.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button brn_settings;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnl_dialer;
+
+
+
+
+
+
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,11 +9,78 @@ using System.Windows.Forms;
 
 namespace OTM_Client
 {
-    public partial class Form1 : Form
+    public partial class frm_main : Form
     {
-        public Form1()
+
+
+        public frm_main()
         {
             InitializeComponent();
+        }
+
+        private void frm_main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnl_default_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
